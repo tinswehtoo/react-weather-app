ed5da5315fac4a0da41e56a22802f7a304c7d63a
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2023-09-15 14:29:22 +0630

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a href="https://tinswehtoo.com" target="_blank">
+            Tin Swe Htoo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/tinswehtoo/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github.
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
