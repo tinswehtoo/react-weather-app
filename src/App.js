1ca473a2c5e624b8dd288adddf50ab186e5fab27
tinswehtoo <141896669+tinswehtoo@users.noreply.github.com>
2023-09-15 14:42:57 +0630

    delete Github url

--- src/App.js
+++ src/App.js
@@ -4,14 +4,6 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by Tin Swe Htoo and is{" "}
-          <a href="https://github.com/tinswehtoo/react-weather-app">
-            open-sourced on Github.
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
